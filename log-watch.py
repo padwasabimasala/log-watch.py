6fd32f375f0fe4a1270a469e9bb46cbc993a8c87
Matthew Thorley <mthorley@nav.com>
2018-08-21 21:21:56 -0600

    comment

--- log-watch.py
+++ log-watch.py
@@ -45,6 +45,32 @@ def tailf(fname):
             yield line.strip()
         time.sleep(0.1)
 
+# 0. Consume an actively written-to w3c-formatted HTTP access log
+# (https://en.wikipedia.org/wiki/Common_Log_Format). 
+# - It should default to reading /var/log/access.log 
+# - and be overridable
+#
+# 1. Display stats every 10s about the traffic during those 10s: 
+# - the sections of the web site with the most hits
+# - interesting summary statistics on # the traffic as a whole. 
+#   A section is defined as being what's before the second '/' in the path. 
+#   For example, the section for "http://my.site.com/pages/create” is
+#   "http://my.site.com/pages". 
+#
+# 2. Make sure a user can keep the app running and monitor the log file continuously 
+#
+# Whenever total traffic for the past 2 minutes exceeds a certain number on average, add
+# a message saying that “High traffic generated an alert - hits = {value},
+# triggered at {time}”. 
+# - The default threshold should be 10 requests per second
+# - it should be overridable.  
+# 
+# 3. Whenever the total traffic drops again below that value on average for the
+# past 2 minutes, print or displays another message detailing when the alert
+# recovered.  
+#
+# 4. Write a test for the alerting logic.
+
 if __name__ == '__main__':
     try:
         fname = sys.argv[1]
